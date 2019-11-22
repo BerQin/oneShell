e7a3985ea42af4160feb741a8005ed2ce29932c8
BerQin <berqin@outlook.com>
2019-11-22 13:33:53 +0800

    ls

--- v2ray.sh
+++ v2ray.sh
@@ -8,12 +8,9 @@ bash <(curl -L -s https://install.direct/go.sh)
 yum install iptables
 yum -y install lrzsz
 
-rz
-
 cp ./iptables /etc/sysconfig/iptables
 cp ./config.json /etc/v2ray/config.json
 
 systemctl restart iptables
 systemctl start v2ray
-
 systemctl status v2ray
