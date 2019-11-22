5fe971959539f0cfc4988d88fea3aaed060f9128
BerQin <berqin@outlook.com>
2019-11-22 13:05:02 +0800

    init commit

---
+++ v2ray.sh
@@ -0,0 +1,17 @@
+v2cpath = '/etc/v2ray/config.json'
+iptabpath = '/etc/sysconfig/iptables'
+
+cp /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
+bash <(curl -L -s https://install.direct/go.sh)
+yum install iptables
+yum -y install lrzsz
+
+rz
+
+cp ./iptables ${iptabpath}
+cp ./config.json ${v2cpath}
+
+systemctl restart iptables
+systemctl start v2ray
+
+systemctl status v2ray
